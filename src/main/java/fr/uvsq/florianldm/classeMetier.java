f336fc56d3268e4d5af18f37e2cc5911361a61ed
= <ledeme.florian@gmail.com>
2020-03-06 19:58:22 +0100

    maj

--- src/main/java/fr/uvsq/florianldm/classeMetier.java
+++ src/main/java/fr/uvsq/florianldm/classeMetier.java
@@ -1,6 +1,12 @@
 package fr.uvsq.florianldm;
 
 public class classeMetier {
-    
+    public classeMetier(){
 
+    }
+
+    public void unemethodedemetier(){
+        //Début fonction
+        //Fin fonction
+    }
 }
